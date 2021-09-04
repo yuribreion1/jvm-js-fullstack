4621482841f6029e73cb0f8187a27c1ac1891b3c
Yuri Breion <yuribreion@br.ibm.com>
2021-09-04 18:26:44 -0300

    fix path

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -12,12 +12,11 @@ import org.litote.kmongo.coroutine.*
 import org.litote.kmongo.reactivestreams.KMongo
 import com.mongodb.ConnectionString
 import io.ktor.features.*
-import kotlinx.serialization.Serializable
 
 fun main() {
 
     val connectionString: ConnectionString? = System.getenv("MONGODB_URI")?.let {
-        ConnectionString("$it?retryWrites=false")
+        ConnectionString("$it")
     }
 
     val client = if (connectionString != null) KMongo.createClient(connectionString).coroutine else KMongo.createClient().coroutine
